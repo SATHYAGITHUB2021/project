8b0d3aaf6a858c53e497981deea5eb5d9f3be42e
SATHYANARAYANA.JOKU <sathyanarayana.joku14@gmail.com>
2021-06-04 21:27:03 +0530

    added all in makefile

--- if.sh
+++ if.sh
@@ -1,10 +1,23 @@
+#intializing two variable
 A=100
 B=200
+#if statement
+#check weather they are equal
+if [ $A == $B ];then
+  echo A is equal to B
+fi
+#check weather they are not equal
+if
+  echo A is not equal to B
+fi
+
+#if else statement
 if [ $A == $B ];then
   echo A is equal to B
 else
   echo A is not equal to B
 fi
+
 
 
 
