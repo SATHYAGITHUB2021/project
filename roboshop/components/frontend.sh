06f3d5667ca73952405c4fe77a8c1a2421a49299
SATHYANARAYANA.JOKU <sathyanarayana.joku14@gmail.com>
2021-06-05 15:02:39 +0530

    downloaded curl from gitgub to frontend.sh

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -9,3 +9,7 @@ STAT $?
 HEAD "Start Nginx\t"
 systemctl enable nginx &>>/tmp/roboshop.log && systemctl start nginx &>>/tmp/roboshop.log
 STAT $?
+
+HEAD "Download from Github"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+STAT $?
