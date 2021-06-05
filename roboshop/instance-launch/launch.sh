3a55b787198003ad37f966ba2a412feec2f932e3
SATHYANARAYANA.JOKU <sathyanarayana.joku14@gmail.com>
2021-06-05 17:56:13 +0530

    added launch.sh file

--- roboshop/instance-launch/launch.sh
+++ roboshop/instance-launch/launch.sh
@@ -3,13 +3,13 @@
 COMPONENT=$1
 ##-z validates the variable empty, true if it is empty.
 if [ -z "${COMPONENT}" ]; then
-  echo "Component is needed"
+  echo "Component is Needed"
   exit 1
 fi
 
 LID=lt-0cc3db256386b7689
 LVER=1
 
-aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}]"
+aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"
 
 
