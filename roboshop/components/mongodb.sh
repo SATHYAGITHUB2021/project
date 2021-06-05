1543991f25dd275ba11aa4091e061eca65683bb6
SATHYANARAYANA.JOKU <sathyanarayana.joku14@gmail.com>
2021-06-05 22:58:49 +0530

    added commands mongodb.sh

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -2,7 +2,7 @@
 source components/common.sh
 rm -f /tmp/roboshop.log
 
-HEAD "Setup mongodb repo files"
+HEAD "Setup mongodb repo files\t"
 echo '[mongodb-org-4.2]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
@@ -11,7 +11,7 @@ enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
 STAT $?
 
-HEAD "Install MongoDB\t"
+HEAD "Install MongoDB\t\t"
 yum install -y mongodb-org &>>/tmp/roboshop.log
 STAT $?
 
@@ -19,7 +19,7 @@ HEAD "Update Listen Address in MongoDB"
 sed -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>/tmp/roboshop.log
 STAT $?
 
-HEAD "Start MongoDB Service\t"
+HEAD "Start MongoDB Service\t\t"
 systemctl enable mongod &>>/tmp/roboshop.log && systemctl restart mongod &>>/tmp/roboshop.log
 STAT $?
 
@@ -27,12 +27,12 @@ HEAD "Download the schema from github"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>/tmp/roboshop.log
 STAT $?
 
-HEAD "Extract downloaded Archive"
+HEAD "Extract downloaded Archive\t"
 cd /tmp
 unzip -o mongodb.zip &>>/tmp/roboshop.log
 STAT $?
 
-HEAD "Load Schema"
+HEAD "Load Schema\t\t\t"
 cd mongodb-main
 mongo < catalogue.js &>>/tmp/roboshop.log && mongo < users.js &>>/tmp/roboshop.log
 STAT $?
