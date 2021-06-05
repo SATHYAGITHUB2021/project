8e3bbf28f72e607c7dcc5cb3736d1a71e26526f7
SATHYANARAYANA.JOKU <sathyanarayana.joku14@gmail.com>
2021-06-05 12:56:09 +0530

    added if command in frontend.sh

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -2,14 +2,13 @@
 
 echo -e "\e[1;31m INSTALLING NGINX ...\e[0m"
 yum install nginx -y &>>/tmp/roboshop.log
-STAT $?
-STAT() {
-  if [ $1 -eq 0 ]; then
+
+if [ $? -eq 0 ]; then
   echo -e "\e[1;32mDone\e[0m"
 else
   echo -e "\e[1;31m Filed\e[0m"
   echo -e "\e[1;32m Check the log for more details...Log-file : /tmp/roboshop.log\e[0m"
   exit 1
 fi
-}
+
 
