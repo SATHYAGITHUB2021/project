9c3d90fa5dfa0f0ea152b1d248bfbaf2a1a92f49
SATHYANARAYANA.JOKU <sathyanarayana.joku14@gmail.com>
2021-06-05 15:00:36 +0530

    added start nginx command in frontend.sh

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -2,6 +2,10 @@
 source components/common.sh
 rm -f /tmp/roboshop.log
 
-HEAD "INSTALLING NGINX"
+HEAD "Installing Nginx"
 yum install nginx -y &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Start Nginx\t"
+systemctl enable nginx &>>/tmp/roboshop.log && systemctl start nginx &>>/tmp/roboshop.log
 STAT $?
