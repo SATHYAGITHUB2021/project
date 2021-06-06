978eb11b1e086c8f7e68df991bb4ca90a8c83cf7
SATHYANARAYANA.JOKU <sathyanarayana.joku14@gmail.com>
2021-06-06 08:16:24 +0530

    added commands catalogue.sh

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -3,15 +3,15 @@
 source components/common.sh
 rm -f /tmp/roboshop.log
 
-HEAD "Install NodeJS"
+HEAD "Install NodeJS\t\t\t"
 yum install nodejs make gcc-c++ -y &>>/tmp/roboshop.log
 STAT $?
 
-HEAD "Add Roboshop App User"
+HEAD "Add Roboshop App User\t\t"
 adduser roboshop &>>/tmp/roboshop.log
 STAT $?
 
-HEAD "Download app from Github"
+HEAD "Download app from Github\t"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>/tmp/roboshop.log
 STAT $?
 
@@ -19,7 +19,7 @@ HEAD "Extract the downloaded Archive "
 cd /home/roboshop && unzip /tmp/catalogue.zip &>>/tmp/roboshop.log && mv catalogue-main catalogue
 STAT $?
 
-HEAD "Install NodeJS Dependencies"
+HEAD "Install NodeJS Dependencies\t"
 cd /home/roboshop/catalogue && npm install --unsafe-perm &>>/tmp/roboshop.log
 STAT $?
 
